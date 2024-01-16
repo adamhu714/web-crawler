28024bf7631a6d553f9e7e2320692a3f6f051bf8
adamhu714 <adamhu714@gmail.com>
2024-01-16 02:13:19 +0000

    crawl js and tests

--- crawl.js
+++ crawl.js
@@ -1,6 +1,12 @@
-function normalizeURL() {
+function normalizeURL(url){
+  const urlObj = new URL(url)
+  fullPath = `${urlObj.hostname}${urlObj.pathname}`
+  if (fullPath.length > 0 && fullPath.slice(-1) === '/') {
+    fullPath = fullPath.slice(0, -1)
+  }
+  return fullPath
 }
 
 module.exports = {
-    normalizeURL
-  }
+  normalizeURL
+}
