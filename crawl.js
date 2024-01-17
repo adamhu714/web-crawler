65274bf7a962636e748a1dbf37da12e89f510d4a
adamhu714 <adamhu714@gmail.com>
2024-01-17 05:09:56 +0000

    Added getURLsFromHTML function

--- crawl.js
+++ crawl.js
@@ -1,4 +1,6 @@
-function normalizeURL(url){
+const { JSDOM } = require('jsdom')
+
+function normalizeURL(url) {
   const urlObj = new URL(url)
   fullPath = `${urlObj.hostname}${urlObj.pathname}`
   if (fullPath.length > 0 && fullPath.slice(-1) === '/') {
@@ -7,6 +9,29 @@ function normalizeURL(url){
   return fullPath
 }
 
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = []
+  const dom = new JSDOM(htmlBody)
+  const aElements = dom.window.document.querySelectorAll('a')
+  for (const aElement of aElements){
+    if (aElement.href.slice(0, 1) === '/'){
+      try {
+        urls.push(new URL(aElement.href, baseURL).href)
+      } catch(err) {
+        console.log(`${err.message}: ${aElement.href}`)
+      }
+    } else {
+      try {
+        urls.push(new URL(aElement.href).href)
+      } catch(err) {
+        console.log(`${err.message}: ${aElement.href}`)
+      }
+    }
+  }
+  return urls
+}
+
 module.exports = {
-  normalizeURL
+  normalizeURL,
+  getURLsFromHTML
 }
