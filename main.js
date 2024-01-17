784e3b8f6a566e57947eed0086de26794f6cfddd
adamhu714 <adamhu714@gmail.com>
2024-01-17 05:57:33 +0000

    Process arguments in main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("Hello world.");
+function main() {
+    if (process.argv.length != 3) {
+        console.log(`Error, please provide exactly 1 URL as a parameter.`)
+    }
+    baseURL = process.argv[2]
+    console.log(`Starting crawl of: ${baseURL}`)
+    return baseURL
+}
+main()
